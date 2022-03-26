d4782709e6255d5935e4374b92fe9d74d9860665
rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>
2022-03-26 10:59:16 +0900

    find_cp

--- ex_prioritization.py
+++ ex_prioritization.py
@@ -9,7 +9,7 @@ from src.make_dag import make_random_dag
 
 ### 課題2個め以降に使っても構いません
 def exec_time_order() -> [int]:
-    order = []
+    ext_order = []
     wait_nodes = [0]
 
     while(len(wait_nodes) != 0):
@@ -19,16 +19,16 @@ def exec_time_order() -> [int]:
                 max_node = node
 
         wait_nodes.remove(max_node)
-        order.append(max_node)
+        ext_order.append(max_node)
         
         for v in G:
-            if v not in order and v not in wait_nodes and {n for n in G.predecessors(v)} <= set(order):
+            if v not in ext_order and v not in wait_nodes and {n for n in G.predecessors(v)} <= set(ext_order):
                 wait_nodes.append(v)
 
-    return order
+    return ext_order
 
 def critical_path_order() -> [int]:
-    order = []
+    cp_order = []
     wait_nodes = [0]
 
     while(len(wait_nodes) != 0):
@@ -41,25 +41,48 @@ def critical_path_order() -> [int]:
                 max_node = node
 
         wait_nodes.remove(max_node)
-        order.append(max_node)
+        cp_order.append(max_node)
         
         for v in G:
-            if v not in order and v not in wait_nodes and {n for n in G.predecessors(v)} <= set(order):
+            if v not in cp_order and v not in wait_nodes and {n for n in G.predecessors(v)} <= set(cp_order):
                 wait_nodes.append(v)
 
-    return order
+    return cp_order
 
 def find_critical_path() -> [int]:
-    return []
+    # 終了時間計算関数
+    def culc_fn_time(idx: int, time):
+        if time > fn_times[idx]:
+            fn_times[idx] = time
+            for suc in G.successors(idx):
+                culc_fn_time(suc, time + G.nodes[suc]["exec"])
+
+    cp = []
+    # 終了時間計算
+    fn_times = [0 for v in G.nodes]
+    culc_fn_time(0, G.nodes[0]["exec"])
+
+    cp_elem = len(G.nodes) - 1
+    cp.append(cp_elem)
+    while(len([v for v in G.predecessors(cp_elem)]) != 0):
+        max_idx = 0
+        for p in G.predecessors(cp_elem):
+            if fn_times[p] > fn_times[max_idx]:
+                max_idx = p
+        cp_elem = max_idx
+        cp.append(cp_elem)
+
+    cp.reverse()
+    return cp
 
 
 
 # DAG定義
 # 0番目が入口ノードであるという前提を使って構いません
-G = make_random_dag(56)
+G = make_random_dag(123)
 
 # クリティカルパス
-critical_path = [0, 4, 6, 7]
+critical_path = find_critical_path()
 
 # 実行順序決定
 order = exec_time_order() # [o for o in G.nodes]
@@ -67,6 +90,7 @@ order = exec_time_order() # [o for o in G.nodes]
 
 # 実行順序確認
 print("order:\t\t"+str(order))
+print("critical_path:\t"+str(critical_path))
 
 
 
