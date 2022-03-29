31221c810a712a1012f8c1624b0521f74067ac72
rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>
2022-03-29 11:43:18 +0900

    add_cpf_template

--- ex_prioritization.py
+++ ex_prioritization.py
@@ -3,6 +3,7 @@ import networkx
 from src.sched_sim import sched_sim
 from src.scheduling_viewer import make_scheduling_view
 from src.make_dag import make_template_dag
+from src.make_dag import make_template_dag2
 from src.make_dag import make_random_dag
 
 
@@ -19,18 +20,31 @@ def find_critical_path() -> [int]:
 
 
 
-# DAG定義
+### DAG定義
 # 0番目が入口ノード、len(G.nodes)-1番目が出繰りノードであるという前提を使って構いません
+
+# 実行時間優先のテンプレート
 G = make_template_dag()
+# クリティカルパス優先のテンプレート
+#G = make_template_dag2()
 # ランダムDAG生成（引数はシード値）
 #G = make_random_dag(123)
 
-# クリティカルパス
-critical_path = [0, 4, 6, 7]
 
-# 実行順序決定
+
+### クリティカルパス
+
+critical_path = [0, 4, 6, 7]
+# 余裕があればクリティカルパス検出アルゴリズムを作成
+#critical_path = ...
+
+
+
+### 実行順序決定
+
 order = [o for o in range(len(G.nodes))]
 # ここで実行順序決定アルゴリズムを書く
+#order = ...
 
 # 実行順序確認
 print("order:\t\t"+str(order))
--- src/make_dag.py
+++ src/make_dag.py
@@ -7,13 +7,31 @@ def make_template_dag():
     G = networkx.DiGraph()
 
     # ノード定義
-    G.add_nodes_from(range(8))
+    nodes = [1, 7, 3, 3, 6, 1, 2, 1]
+    G.add_nodes_from(range(len(nodes)))
     # 最悪実行時間定義
-    for i, exec_time in enumerate([1, 7, 3, 3, 6, 1, 2, 1]):
+    for i, exec_time in enumerate(nodes):
         G.nodes[i]["exec"] = exec_time
 
     # エッジ定義
     G.add_edges_from([(0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (4, 6), (5, 6), (1, 7), (2, 7), (3, 7), (6, 7)])
+    for edge in G.edges:
+        G.edges[edge]["comm"] = 0
+    
+    return G
+
+def make_template_dag2():
+    G = networkx.DiGraph()
+
+    # ノード定義
+    nodes = [1, 3, 2, 1, 1, 4, 1, 2, 1]
+    G.add_nodes_from(range(len(nodes)))
+    # 最悪実行時間定義
+    for i, exec_time in enumerate(nodes):
+        G.nodes[i]["exec"] = exec_time
+
+    # エッジ定義
+    G.add_edges_from([(0, 1), (0, 2), (0, 3), (1, 4), (2, 8), (3, 5), (3, 6), (4, 8), (5, 7), (6, 7), (7, 8)])
     for edge in G.edges:
         G.edges[edge]["comm"] = 0
     
